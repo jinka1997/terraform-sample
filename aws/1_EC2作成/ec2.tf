13aa33d0eb632896a0911db0e9528746f3b53271
Hideki Yoshikura <air2020@HidekinoMacBook-Air.local>
2021-01-02 14:07:59 +0900

    fix

--- aws/1_EC2作成/ec2.tf
+++ aws/1_EC2作成/ec2.tf
@@ -1,59 +1,26 @@
-/*
-data "aws_ami" "windows_server" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["amzn-ami-hvm-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "block-device-mapping.volume-type"
-    values = ["gp2"]
-  }
-}
-*/
-
-resource "aws_instance" "for-study" {
-  ami           = "ami-014612c2d9afaf1ac"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.main.id
+resource "aws_instance" "sample" {
+  ami                         = "ami-014612c2d9afaf1ac" #Microsoft Windows Server 2019 Base
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.sample.id
   associate_public_ip_address = true
-  key_name = "keypair-for-study"
-  security_groups = [aws_security_group.allow_rdp.id]
+  key_name                    = "ec2-sample"
+  security_groups             = [aws_security_group.allow_rdp.id]
   ebs_block_device {
-    device_name           = "/dev/sda1"
-    volume_size           = 30
-    volume_type           = "standard"
+    device_name = "/dev/sda1"
+    volume_size = 30
+    volume_type = "standard"
   }
 
   tags = {
-    Name = "tf-study-123"
+    Name = "ec2-sample"
   }
 }
-
-
 
 resource "aws_security_group" "allow_rdp" {
   name        = "allow_rdp"
   description = "Allow RDP from my home"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.sample.id
 
   ingress {
     description = "Remoter desktop"
