456221ca1590895d0c98228b75935251c31e0767
Hideki Yoshikura <evenintherain@gmail.com>
2021-03-18 12:02:47 +0900

    fix

--- aws/8_StepFunctions+EventBridge/stepfunctions.tf
+++ aws/8_StepFunctions+EventBridge/stepfunctions.tf
@@ -18,23 +18,26 @@
         }
     ) 
     name          = "MyStateMachine" 
-    role_arn      = "arn:aws:iam::581057229408:role/service-role/StepFunctions-MyStateMachine-role-a7396e26" 
-    tags          = {} 
+    role_arn      = aws_iam_role.sample_sfn_role.arn
+    tags          = {
+
+    } 
     type          = "STANDARD" 
 
     logging_configuration {
       include_execution_data = true 
       level                  = "ALL" 
-      log_destination        = "arn:aws:logs:ap-northeast-1:581057229408:log-group:/aws/vendedlogs/states/MyStateMachine-Logs:*" 
+      log_destination        = format("%s:*", aws_cloudwatch_log_group.sample_log_group.arn)
+      #log_destination        = "arn:aws:logs:ap-northeast-1:581057229408:log-group:/aws/vendedlogs/states/MyStateMachine-Logs:*" 
     }
 }
 
 resource "aws_cloudwatch_event_rule" "sample_rule" {
   description         = "aa" 
   event_bus_name      = "default"
-  is_enabled          = true 
+  is_enabled          = true
   name                = "exec_stepfunctions" 
-  schedule_expression = "cron(13 16 * * ? *)" 
+  schedule_expression = "cron(55 1 * * ? *)" 
   tags                = {} 
 }
 
@@ -42,11 +45,81 @@ resource "aws_cloudwatch_event_rule" "sample_rule" {
 resource "aws_cloudwatch_event_target" "sample-event-target" {
   arn            = aws_sfn_state_machine.sample_sfn.arn
   event_bus_name = "default" 
-  role_arn       = "arn:aws:iam::581057229408:role/service-role/Amazon_EventBridge_Invoke_Step_Functions_780854943"
+  role_arn       = aws_iam_role.sample_event_invoke_role.arn
   rule           = "exec_stepfunctions" 
-  #target_id      = "Id2b6017c2-9109-4832-b977-d0f23f6b4204" 
   depends_on = [
     aws_cloudwatch_event_rule.sample_rule
   ]
 }
 
+
+resource "aws_iam_role" "sample_sfn_role" {
+  assume_role_policy    = jsonencode(
+      {
+        Statement = [
+          {
+            Action    = "sts:AssumeRole"
+            Effect    = "Allow"
+            Principal = {
+              Service = "states.amazonaws.com"
+            }
+          },
+        ]
+        Version   = "2012-10-17"
+      }
+  )
+  force_detach_policies = false 
+  managed_policy_arns   = [
+    "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess",
+    "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess",
+  ]
+  max_session_duration  = 3600 
+  name                  = "Sample-Exec-StepFunctions-MyStateMachine-role"
+  path                  = "/service-role/"
+  tags                  = {
+
+  } 
+  inline_policy {
+
+  }
+}
+
+resource "aws_iam_role" "sample_event_invoke_role" {
+  assume_role_policy    = jsonencode(
+      {
+        Statement = [
+          {
+            Action    = "sts:AssumeRole"
+            Effect    = "Allow"
+            Principal = {
+              Service = "events.amazonaws.com"
+            }
+          },
+        ]
+        Version   = "2012-10-17"
+      }
+  ) 
+  force_detach_policies = false 
+  managed_policy_arns   = [
+    "arn:aws:iam::aws:policy/AWSStepFunctionsConsoleFullAccess"
+  ] 
+  max_session_duration  = 3600 
+  name                  = "Sample_Amazon_EventBridge_Invoke_Step_Functions_MyStateMachine" 
+  path                  = "/service-role/" 
+  tags                  = {
+
+  } 
+  inline_policy {
+
+  }
+}
+
+resource "aws_cloudwatch_log_group" "sample_log_group" {
+  #arn               = "arn:aws:logs:ap-northeast-1:581057229408:log-group:/aws/vendedlogs/states/MyStateMachine-Logs" -> null
+  #id                = "/aws/vendedlogs/states/MyStateMachine-Logs" -> null
+  name              = "/aws/vendedlogs/states/MyStateMachine-Logs" 
+  retention_in_days = 0 
+  tags              = {
+
+  } 
+}
